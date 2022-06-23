60279d3a6b30e507ae4ef7edfd07a0ba64e1e4a2
oscar carranza <oscarcarranza504@gmail.com>
2022-06-23 16:54:16 -0600

    Add data Time Navbar

--- src/components/Client/NavBar/index.js
+++ src/components/Client/NavBar/index.js
@@ -1,24 +1,43 @@
 import FacebookSVG from 'components/Icons/Facebook'
+import InstagramSVG from 'components/Icons/Instagram'
 import SettingsSVG from 'components/Icons/Settings'
+import TwitterSVG from 'components/Icons/twitter'
 import YoutubeSVG from 'components/Icons/Youtube'
 import Search from 'components/Search'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import Categories from './categories'
 import styles from './nav.module.css'
+import useFormatDate from 'hooks/useFormatDate'
 
 export default function Navbar() {
+  const [time, setTime] = useState('')
   const categories = Categories()
+  useEffect(() => {
+    const updateTime = setInterval(() => {
+      const obtainData = new Date().toISOString()
+      const formatTime = useFormatDate(obtainData, false)
+      setTime(formatTime)
+    }, 1000)
+    console.log(time)
+    return () => clearInterval(updateTime)
+  }, [])
   return (
     <>
       <div className="flex justify-between">
         <div>
-          <p>35° Choluteca</p>
+          <div className="flex items-end">
+            <img src="/116.webp" alt="wheater" className="w-12" />
+            <p className="text-5xl font-bold">36</p>
+            <span className="text-base">°C</span>
+          </div>
+          <p className="text-sm text-center">Choluteca, HN</p>
         </div>
         <div className="w-8/12">
           <div className="bg-slate-400 w-full h-10"></div>
         </div>
-        <div>11:45 am</div>
+        <div>{time}</div>
       </div>
       <header className={styles.Navbar}>
         <div className="relative w-full flex items-center justify-between p-2">
@@ -42,17 +61,30 @@ export default function Navbar() {
           </div>
         </div>
       </header>
-      <div className="flex justify-between">
-        <div>
-          <FacebookSVG width={42} height={42} />
-          <YoutubeSVG />
+      <div className="flex items-center mb-5 p-2 bg-slate-200">
+        <div className={styles.socialMedia}>
+          <a href="#" target="_blank" rel="noreferrer">
+            <FacebookSVG size={24} />
+          </a>
+          <a href="#" target="_blank" rel="noreferrer">
+            <YoutubeSVG size={24} />
+          </a>
+          <a href="#" target="_blank" rel="noreferrer">
+            <InstagramSVG size={24} />
+          </a>
+          <a href="#" target="_blank" rel="noreferrer">
+            <TwitterSVG size={24} />
+          </a>
         </div>
-        <div className="flex flex-wrap  mt-1">
+        <div className="w-1 h-8 bg-slate-500 ml-3 mr-1 rounded opacity-80">
+          <span className="opacity-0">|</span>
+        </div>
+        <div className="flex flex-wrap  mt-1 ">
           {categories.map((category) => {
             return (
               <div key={category.id} className={styles.categoryLinks}>
-                <Link href={category.href}>
-                  <a className="font-semibold p-1">{category.name}</a>
+                <Link href={'/[category]'} as={`/${category.href}`}>
+                  <a className="font-semibold p-1 text-sm">{category.name}</a>
                 </Link>
               </div>
             )
--- src/components/Icons/Facebook.js
+++ src/components/Icons/Facebook.js
@@ -1,34 +1,19 @@
 const FacebookSVG = ({ size }) => (
   <svg
-    clipRule="evenodd"
-    fillRule="evenodd"
-    imageRendering="optimizeQuality"
-    shapeRendering="geometricPrecision"
-    textRendering="geometricPrecision"
-    viewBox="17006.82 17511.1 8157.8 8157.8"
     xmlns="http://www.w3.org/2000/svg"
-    height={size}
+    viewBox="0 0 455.73 455.73"
+    style={{
+      enableBackground: 'new 0 0 455.73 455.73'
+    }}
+    xmlSpace="preserve"
     width={size}
+    height={size}
   >
-    <linearGradient
-      id="a"
-      gradientUnits="userSpaceOnUse"
-      x1={21085.72}
-      x2={21085.72}
-      y1={18249.39}
-      y2={25150.62}
-    >
-      <stop offset={0} stopColor="#00b2ff" />
-      <stop offset={1} stopColor="#006aff" />
-    </linearGradient>
     <path
-      d="M21085.72 18309.17c1811.95 0 3280.83 1468.88 3280.83 3280.83s-1468.88 3280.83-3280.83 3280.83-3280.83-1468.88-3280.83-3280.83 1468.88-3280.83 3280.83-3280.83z"
-      fill="url(#a)"
-    />
-    <path
-      d="M21512.01 24843.29v-2534.17h714.43l94.7-891.91h-809.13l1.2-446.44c0-232.63 22.1-357.22 356.24-357.22h446.68v-892.06h-714.59c-858.35 0-1160.42 432.65-1160.42 1160.34v535.45h-535.07v891.99h535.07v2498.09c208.45 41.53 423.95 63.47 644.6 63.47a3310.9 3310.9 0 0 0 426.29-27.54z"
-      fill="#fff"
-      fillRule="nonzero"
+      style={{
+        fill: '#3a559f'
+      }}
+      d="M0 0v455.73h242.704V279.691h-59.33v-71.864h59.33v-60.353c0-43.893 35.582-79.475 79.475-79.475h62.025v64.622h-44.382c-13.947 0-25.254 11.307-25.254 25.254v49.953h68.521l-9.47 71.864h-59.051V455.73H455.73V0H0z"
     />
   </svg>
 )
