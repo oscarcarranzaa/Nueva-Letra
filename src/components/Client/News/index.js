f49cb29bb1ee134502b7f8f72ea5f8422115feff
oscar carranza <oscarcarranza504@gmail.com>
2022-06-30 11:54:16 -0600

    Refactor home

--- src/components/Client/News/index.js
+++ src/components/Client/News/index.js
@@ -1,45 +1,49 @@
-import useFormatDate from 'hooks/useFormatDate'
 import Image from 'next/image'
 import Link from 'next/link'
+import useCategoryID from 'hooks/useCategoryID'
+import useFormatDate from 'hooks/useFormatDate'
 import styles from './News.module.css'
 
 export default function NewsData({ news }) {
   return (
     <>
-      <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-        {news.map((res) => {
-          const date = useFormatDate(
-            res.createdAt,
-            false,
-            'D MMMM YYYY, h:mm a',
-            true
-          )
-          return (
-            <Link
-              href={'/[category]/[id]'}
-              as={`/${res.category}/${res.id}`}
-              key={res.id}
-            >
-              <a className={styles.linkNews}>
-                <figure className={styles.figureImage}>
-                  <Image
-                    src={res.image}
-                    width={200}
-                    height={150}
-                    layout="responsive"
-                    objectFit="cover"
-                  />
-                  <div className={styles.imageOpacity}></div>
-                  <p className="text-sm font-semibold line-clamp-3 absolute bottom-0 ">
-                    {res.title}
-                  </p>
-                </figure>
-                <p className="text-xs font-semibold">{date}</p>
-              </a>
-            </Link>
-          )
-        })}
-      </div>
+      {news.filter(Boolean).map((res) => {
+        const date = useFormatDate(
+          res.createdAt,
+          false,
+          'D MMMM YYYY, h:mm a',
+          true
+        )
+        const category = useCategoryID(res.category_code)
+        const categoryValue = category[0].value
+        const categoryName = category[0].name
+        return (
+          <Link
+            href={'/[category]/[id]'}
+            as={`/${categoryValue}/${res.id}`}
+            key={res.id}
+          >
+            <a className={styles.linkNews}>
+              <figure className={styles.figureImage}>
+                <Image
+                  src={res.image}
+                  width={150}
+                  height={100}
+                  layout="responsive"
+                  objectFit="cover"
+                />
+              </figure>
+              <p className="text-base font-semibold line-clamp-2 ">
+                {res.title}
+              </p>
+              <div className="flex text-xs font-semibold justify-between text-slate-800">
+                <p>{date}</p>
+                <p className="capitalize">{categoryName}</p>
+              </div>
+            </a>
+          </Link>
+        )
+      })}
     </>
   )
 }
