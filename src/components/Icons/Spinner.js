3e53a057f9f92f22bdd4c4a18f82539a92c1bedd
oscar carranza <oscarcarranza504@gmail.com>
2022-07-21 14:39:14 -0600

    Styled login

--- src/components/Icons/Spinner.js
+++ src/components/Icons/Spinner.js
@@ -7,7 +7,7 @@ const Spinner = ({ primaryFill, secondFill, size }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      opacity={0.2}
+      opacity={0.3}
       fillRule="evenodd"
       clipRule="evenodd"
       d="M12 19a7 7 0 1 0 0-14 7 7 0 0 0 0 14Zm0 3c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z"
--- src/pages/dash/login.js
+++ src/pages/dash/login.js
@@ -2,7 +2,12 @@ import axios from 'axios'
 import Close from 'components/Icons/Close'
 import EyeSVG from 'components/Icons/Eye'
 import EyeInvisibleSVG from 'components/Icons/EyeInvisible'
+import Spinner from 'components/Icons/Spinner'
+import WarnSVG from 'components/Icons/Warn'
 import isValidEmail from 'hooks/useValidEmail'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import styles from 'styles/login.module.css'
@@ -13,6 +18,10 @@ export default function Login() {
   const [pass, setPass] = useState('')
   const [validPass, setValidPass] = useState(true)
   const [watchPass, setWatchPass] = useState(false)
+  const [error, setError] = useState(null)
+  const [send, setSend] = useState(false)
+  const [redirect, setRedirect] = useState('Vamos')
+
   const Router = useRouter()
   const validateEmail = (email) => {
     const validate = isValidEmail(email)
@@ -21,17 +30,27 @@ export default function Login() {
   const sendLogin = (e) => {
     e.preventDefault()
     const validateEmail = isValidEmail(email)
-
+    email.length <= 3 ? setValidEmail(false) : setValidEmail(true)
+    pass.length <= 4 ? setValidPass(false) : setValidPass(true)
     if (validateEmail && pass.length > 4) {
+      setSend(true)
       const credentials = { email, password: pass }
       axios({
         method: 'POST',
         url: '/auth/login',
         data: credentials,
         withCredentials: true
-      }).then(() => {
-        Router.push('/dash')
       })
+        .then(() => {
+          setSend(false)
+          setRedirect('Redireccionando...')
+          setError(null)
+          Router.push('/dash')
+        })
+        .catch((err) => {
+          setError(err.response.data.response)
+          setSend(false)
+        })
     }
   }
   const emailSucces = validEmail ? null : 'Correo no válido'
@@ -39,11 +58,53 @@ export default function Login() {
   const inputPass = watchPass ? 'text' : 'password'
   return (
     <>
-      <div className="flex justify-center items-center w-full h-screen bg-zinc-800">
+      <Head>
+        <title>Login - CDM</title>
+      </Head>
+      <header className={styles.header}>
+        <div className="w-11/12 flex justify-between max-w-7xl">
+          <div>
+            <Link href={'/'}>
+              <a>
+                <Image
+                  src={'/static/images/CDM_logo.png'}
+                  width={100}
+                  height={50}
+                  objectFit="cover"
+                ></Image>
+              </a>
+            </Link>
+          </div>
+          <div className="text-white flex items-center">
+            <Link href={'/'}>Inicio</Link>
+            <a
+              href="https://wa.me/32319828?text=ayuda"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-5 bg-blue-800 p-1 pr-3 pl-3 rounded border border-transparent hover:border-slate-400"
+            >
+              Soporte
+            </a>
+          </div>
+        </div>
+      </header>
+      <div className={styles.formContainer}>
         <form onSubmit={sendLogin} className={styles.form}>
-          <h2 className="text-lg font-semibold mb-5 pl-2 pr-2">
+          <h2
+            className={`text-lg font-semibold pl-2 pr-2 ${
+              error !== null ? 'mb-1' : 'mb-9'
+            }`}
+          >
             Iniciar sesión en CDM - Panel
           </h2>
+          <div
+            className={`${styles.msgErrorLogin} ${
+              error !== null ? 'flex' : 'hidden'
+            }`}
+          >
+            <WarnSVG width={15} height={15} />
+            <small className="ml-1">{error}</small>
+          </div>
           <div className={styles.inputCont}>
             <p>Correo electrónico</p>
             <div
@@ -110,7 +171,16 @@ export default function Login() {
               <p>{passSucces}</p>
             </div>
           </div>
-          <button className={styles.buttonSend}>Vamos</button>
+          <button
+            className={`${styles.buttonSend} ${send ? styles.sendLogin : ''}`}
+            disabled={send}
+          >
+            {!send ? (
+              redirect
+            ) : (
+              <Spinner size={24} primaryFill={'#fff'} secondFill={'#fff'} />
+            )}
+          </button>
         </form>
       </div>
     </>
