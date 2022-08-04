45997e88e048925881ff936f3d449d16db5723b4
oscar carranza <oscarcarranza504@gmail.com>
2022-08-04 16:51:10 -0600

    ADD backend

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,8 @@ import '../styles/globals.css'
 import '../components/Client/Slider/slick.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.BACKEND_API + '/api/v1'
+axios.defaults.baseURL = 'https://diario-cdm-api.vercel.app/api/v1'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
 export default MyApp
